723f096bc36164f09cc6662fccb56e2bc2ae3f8b
qthequartermasterman <qthequartermasterman@gmail.com>
2024-04-20 21:33:02 -0500

    feat: dtype strategies

---
+++ hypothesis_torch/dtype.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import torch
+from hypothesis import strategies as st
+from typing import Sequence
+import numpy as np
+
+SIGNED_INT_DTYPES = [torch.int8, torch.int16, torch.int32, torch.int64]
+"""All signed integer dtypes supported by PyTorch."""
+UNSIGNED_INT_DTYPES = [torch.uint8]
+"""All unsigned integer dtypes supported by PyTorch."""
+INT_DTYPES = SIGNED_INT_DTYPES + UNSIGNED_INT_DTYPES
+"""All integer (both signed and unsigned) dtypes supported by PyTorch."""
+
+FLOAT_DTYPES = [torch.float16, torch.float32, torch.float64]
+"""All floating point dtypes supported by PyTorch."""
+BFLOAT_DTYPES = [torch.bfloat16]
+"""All brain-float dtypes supported by PyTorch."""
+COMPLEX_DTYPES = [torch.complex64, torch.complex128]
+"""All complex dtypes supported by PyTorch."""
+BOOL_DTYPES = [torch.bool]
+"""All boolean dtypes supported by PyTorch."""
+
+ALL_DTYPES = INT_DTYPES + FLOAT_DTYPES + BFLOAT_DTYPES + COMPLEX_DTYPES + BOOL_DTYPES
+"""All dtypes supported by PyTorch."""
+
+numpy_dtype_map: dict[torch.dtype, np.dtype] = {
+    torch.int8: np.int8,
+    torch.int16: np.int16,
+    torch.int32: np.int32,
+    torch.int64: np.int64,
+    torch.uint8: np.uint8,
+    torch.float16: np.float16,
+    torch.float32: np.float32,
+    torch.float64: np.float64,
+    torch.bfloat16: np.float32,  # bfloat16 is not supported by numpy
+    torch.complex64: np.complex64,
+    torch.complex128: np.complex128,
+    torch.bool: np.bool_,
+}
+"""A mapping from torch dtypes to numpy dtypes. Useful for generating tensors of arbitrary dtypes from the builtin numpy strategies."""
+
+assert set(numpy_dtype_map.keys()) == set(ALL_DTYPES)
+
+
+@st.composite
+def dtype_strategy(draw: st.DrawFn, *, elements: Sequence[torch.dtype] | None = None):
+    """Strategy for generating torch dtypes.
+
+    Args:
+        draw: The draw function provided by `hypothesis`. If `None`, all dtypes are sampled.
+        elements: A strategy for generating elements of the dtype.
+
+    Returns:
+        A strategy for generating torch dtypes.
+    """
+    if elements is None:
+        elements = ALL_DTYPES
+    return draw(st.sampled_from(elements))
+
+st.register_type_strategy(torch.dtype, dtype_strategy())
---
+++ tests/unit/test_dtype.py
@@ -0,0 +1,31 @@
+import unittest
+
+import hypothesis
+import torch
+
+import hypothesis_torch.dtype
+
+class TestDtype(unittest.TestCase):
+    @hypothesis.given(dtype=...)
+    def test_dtype_strategy(self, dtype: torch.dtype):
+        """Test that using the registered `torch.dtype` strategy gives a dtype."""
+        self.assertIsInstance(dtype, torch.dtype)
+        self.assertIn(dtype, hypothesis_torch.dtype.ALL_DTYPES)
+
+    @hypothesis.given(dtype=hypothesis_torch.dtype.dtype_strategy(elements=hypothesis_torch.dtype.INT_DTYPES))
+    def test_dtype_strategy_int(self, dtype: torch.dtype):
+        """Test that the dtype strategy when specifying a list of dtypes only generates dtypes in that list."""
+        self.assertIsInstance(dtype, torch.dtype)
+        self.assertIn(dtype, hypothesis_torch.dtype.INT_DTYPES)
+
+    @hypothesis.given(dtype=hypothesis_torch.dtype.dtype_strategy(elements=hypothesis_torch.dtype.FLOAT_DTYPES))
+    def test_dtype_strategy_float(self, dtype: torch.dtype):
+        """Test that the dtype strategy when specifying a list of dtypes only generates dtypes in that list."""
+        self.assertIsInstance(dtype, torch.dtype)
+        self.assertIn(dtype, hypothesis_torch.dtype.FLOAT_DTYPES)
+
+    @hypothesis.given(dtype=hypothesis_torch.dtype.dtype_strategy())
+    def test_dtype_strategy_default(self, dtype: torch.dtype):
+        """Test that the default arguments of dtype strategy can generate any dtype."""
+        self.assertIsInstance(dtype, torch.dtype)
+        self.assertIn(dtype, hypothesis_torch.dtype.ALL_DTYPES)
