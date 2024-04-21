eaa7a78d5a8fdcccc35f577c33d4b50516d9efcb
qthequartermasterman <qthequartermasterman@gmail.com>
2024-04-21 14:02:22 -0500

    feat: import strategies into the root

--- hypothesis_torch/__init__.py
+++ hypothesis_torch/__init__.py
@@ -1,3 +1,5 @@
 __version__ = "0.0.1"
-from hypothesis_torch.tensor import *
+from hypothesis_torch.tensor import tensor_strategy
+from hypothesis_torch.device import device_strategy
+from hypothesis_torch.dtype import dtype_strategy
 from hypothesis_torch.module import *
