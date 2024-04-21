b3142037e069fad2f4a659296c350558a58e9fd7
qthequartermasterman <qthequartermasterman@gmail.com>
2024-04-21 14:18:34 -0500

    feat: import transformer strategy into root if and only if transformers is installed

--- hypothesis_torch/__init__.py
+++ hypothesis_torch/__init__.py
@@ -3,3 +3,10 @@ from hypothesis_torch.tensor import tensor_strategy
 from hypothesis_torch.device import device_strategy
 from hypothesis_torch.dtype import dtype_strategy
 from hypothesis_torch.module import *
+
+try:
+    # Import Hugging Face strategies if transformers is installed
+    import transformers
+    from hypothesis_torch.huggingface import transformer_strategy
+except ImportError:
+    pass
