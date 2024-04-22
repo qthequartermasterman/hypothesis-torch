50da77539f9d4d30165d6f136d46518b7991b00d
qthequartermasterman <qthequartermasterman@gmail.com>
2024-04-21 21:20:23 -0500

    test: :white_check_mark: unit tests for tensors

--- tests/unit/test_tensor.py
+++ tests/unit/test_tensor.py
@@ -1,10 +1,40 @@
 import unittest
+from typing import Any
+
 import hypothesis
 import torch
+import hypothesis_torch
+from tests.unit import utils
+from hypothesis import strategies as st
 
 
 class TestTensor(unittest.TestCase):
+    # TODO: Figure out a way to test passing in strategies into the arguments of the tensor_strategy.
+
     @hypothesis.given(tensor=...)
     def test_tensor_strategy(self, tensor: torch.Tensor) -> None:
         """Test that the registered tensor strategy generates tensors."""
         self.assertIsInstance(tensor, torch.Tensor)
+
+    @hypothesis.given(
+        tensor_and_kwargs=utils.meta_strategy_constraints(
+            strategy_func=hypothesis_torch.tensor.tensor_strategy,
+            dtype=hypothesis_torch.dtype_strategy(),
+            shape=st.lists(st.integers(min_value=1, max_value=10), min_size=1, max_size=3),
+            device=hypothesis_torch.device_strategy(),
+        )
+    )
+    def test_tensor_strategy(self, tensor_and_kwargs: tuple[torch.Tensor, dict[str, Any]]) -> None:
+        """Test that the registered tensor strategy generates tensors."""
+        tensor, kwargs = tensor_and_kwargs
+        self.assertIsInstance(tensor, torch.Tensor)
+        self.assertEqual(tensor.dtype, kwargs["dtype"])
+        self.assertEqual(tensor.shape, torch.Size(kwargs["shape"]))
+
+        # Device equality is not directly comparable. We can compare the device type and index.
+        # Device index 0 is considered the same as index `None`.
+        self.assertEqual(tensor.device.type, kwargs["device"].type)
+        if tensor.device.index in (0, None):
+            assert kwargs["device"].index in (0, None)
+        else:
+            self.assertEqual(tensor.device.index, kwargs["device"].index)
---
+++ tests/unit/utils.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+
+from hypothesis import strategies as st
+
+from typing_extensions import ParamSpec, Callable, TypeVar
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+@st.composite
+def meta_strategy_constraints(
+    draw: st.DrawFn, strategy_func: Callable[P, st.SearchStrategy[T]], **kwargs: dict[str, Any | st.SearchStrategy[Any]]
+) -> tuple[T, dict[str, Any]]:
+    """A strategy that takes another strategy, and allows specifying constraints on that strategy."""
+    for key, value in kwargs.items():
+        if isinstance(value, st.SearchStrategy):
+            kwargs[key] = draw(value)
+    strategy = strategy_func(**kwargs)
+    return draw(strategy), kwargs
