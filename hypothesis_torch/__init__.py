2f9395f7749a2914dc586471e8fe3d86fd3904f5
qthequartermasterman <qthequartermasterman@gmail.com>
2024-04-22 21:45:18 -0500

    docs: update root docstring

--- hypothesis_torch/__init__.py
+++ hypothesis_torch/__init__.py
@@ -1,4 +1,8 @@
-"""Root imports for hypothesis-torch."""
+"""Hypothesis strategies for various Pytorch structures (including tensors and modules).
+
+[Hypothesis](https://hypothesis.readthedocs.io/en/latest/) is a powerful property-based testing library for Python. It
+lacks built-in support for Pytorch tensors and modules, so this library provides strategies for generating them.
+"""
 
 __version__ = "0.1.10"
 import importlib.util
