7ea76aa612b3ff22a79a25cefc7726ace7c96c76
semantic-release <semantic-release>
2024-05-09 02:58:33 +0000

    0.5.0
    Automatically generated by python-semantic-release

--- hypothesis_torch/__init__.py
+++ hypothesis_torch/__init__.py
@@ -4,7 +4,7 @@
 lacks built-in support for Pytorch tensors and modules, so this library provides strategies for generating them.
 """
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 import importlib.util
 
 from hypothesis_torch.device import (
