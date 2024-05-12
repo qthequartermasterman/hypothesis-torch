fd62acad596925d0ae58187767452df1bd6b9350
qthequartermasterman <qthequartermasterman@gmail.com>
2024-05-11 22:01:48 -0500

    ci: store hypothesis database in GitHub Actions to allow reproducing locally

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,30 @@
 """Global test configuration."""
 
+import datetime
+
 import hypothesis
 import os
+from hypothesis import database
 
-hypothesis.settings.register_profile("ci", max_examples=100)
-hypothesis.settings.register_profile("slow", max_examples=1000)
-hypothesis.settings.register_profile("dev", max_examples=10)
-hypothesis.settings.register_profile("fast", max_examples=3)
-hypothesis.settings.register_profile("debug", max_examples=10, verbosity=hypothesis.Verbosity.verbose)
+
+local = database.DirectoryBasedExampleDatabase(".hypothesis/examples")
+shared = database.ReadOnlyDatabase(
+    database.GitHubArtifactDatabase(
+        "qthequartermaserman",
+        "hypothesis-torch",
+        cache_timeout=datetime.timedelta(days=7),
+    )
+)
+
+hypothesis.settings.register_profile("ci", max_examples=100, database=local)
+hypothesis.settings.register_profile("default", max_examples=100, database=database.MultiplexedDatabase(local, shared))
+hypothesis.settings.register_profile("slow", max_examples=1000, database=database.MultiplexedDatabase(local, shared))
+hypothesis.settings.register_profile("dev", max_examples=10, database=database.MultiplexedDatabase(local, shared))
+hypothesis.settings.register_profile("fast", max_examples=3, database=database.MultiplexedDatabase(local, shared))
+hypothesis.settings.register_profile(
+    "debug",
+    max_examples=10,
+    verbosity=hypothesis.Verbosity.verbose,
+    database=database.MultiplexedDatabase(local, shared),
+)
 hypothesis.settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
